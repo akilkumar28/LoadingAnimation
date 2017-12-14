7e99ea5c9c9b04833517c8c89d2b8dd0297ec46f
akilkumar28 <akil.psn@gmail.com>
2017-12-14 00:59:12 -0700

    circle animation completed

--- LoadingAnimation/ViewController.swift
+++ LoadingAnimation/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var shape:CAShapeLayer!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //track layer
+        
+        let trackLayer = CAShapeLayer()
+        let centre = view.center
+        let path = UIBezierPath(arcCenter: centre, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2*CGFloat.pi, clockwise: true)
+        trackLayer.path = path.cgPath
+        trackLayer.strokeColor = UIColor.lightGray.cgColor
+        trackLayer.lineWidth = 10.0
+        trackLayer.fillColor =  UIColor.clear.cgColor
+        view.layer.addSublayer(trackLayer)
+        
+        //shape layer
+        shape = CAShapeLayer()
+        shape.path = path.cgPath
+        shape.strokeColor = UIColor.red.cgColor
+        shape.lineWidth = 10.0
+        shape.strokeEnd = 0
+        shape.fillColor =  UIColor.clear.cgColor
+        view.layer.addSublayer(shape)
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(viewTapped)))
+        
+    }
+    
+    @objc func viewTapped() {
+        
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.toValue = 1
+        animation.duration = 3
+        animation.isRemovedOnCompletion = false
+        animation.fillMode = kCAFillModeForwards
+        shape.add(animation, forKey: "someAnimation")
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
